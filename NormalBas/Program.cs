d8a387ae54a43d75a75e815e75bf3eaabf6e60d7
Алексей Василенко <barbaross1998@gmail.com>
2019-01-09 04:21:39 +0200

    with some func

--- NormalBas/Program.cs
+++ NormalBas/Program.cs
@@ -5,8 +5,67 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace NormalBas {
-    class Program {
+    public class Program {
         static void Main(string[] args) {
+
+
         }
+
+        public static int[] StrToByt(string a) {
+            var bit = a.Length;
+            int[] n = new int[bit];
+
+            for (var i = 0; i < a.Length; i++) {
+                n[a.Length - 1 - i] = Convert.ToByte(a.Substring(a.Length - (i + 1), 1), 2);
+            }
+
+            return n;
+        }
+
+        public static string BytToStr(int[] a) {
+            StringBuilder b = new StringBuilder();
+
+            for (int i = 0; i < a.Length; i++) {
+                b.Append(Convert.ToString(a[i], 2));
+            }
+
+            return b.ToString();
+        }
+
+        public static int[] Add(int[] a, int[] b) {
+            int max;
+            if (a.Length < b.Length) { max = b.Length; }
+            else { max = a.Length; };
+
+            Array.Resize(ref a, max);
+            Array.Resize(ref b, max);
+            int[] res = new int[a.Length];
+
+            for (int i = 0; i < a.Length; i++) {
+                res[i] = (a[i] ^ b[i]);
+            }
+
+            return res;
+        }
+
+        public static int[] ShiftBitToR(int[] a)
+        {
+            int[] res = new int[a.Length];
+            for (int i = 0; i < a.Length - 1; i++) {
+                res[i + 1] = a[i];
+            }
+            res[0] = a[a.Length - 1];
+            return res;
+        }
+
+        public static int Tr(int[] a) {
+            int tr = 0;
+            for (int i = 0; i < a.Length; i++)
+                tr = tr ^ a[i];
+            return tr;
+        }
+
+       
+
     }
 }
